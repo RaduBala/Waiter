a91dedc5ff8b00a7ab8ec2c7a74a54bbfb813b59
Radu Balauroiu <balauroiu.radu@yahoo.com>
2020-11-06 16:35:06 +0200

    Add tabbed menu to the bottom of the screen

--- Waiter/Waiter/Views/TabbedMenuPage.xaml.cs
+++ Waiter/Waiter/Views/TabbedMenuPage.xaml.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 using Xamarin.Forms.Xaml;
 
 namespace Waiter.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class TabbedMenuPage : TabbedPage
+    public partial class TabbedMenuPage : Xamarin.Forms.TabbedPage
     {
         public TabbedMenuPage()
         {
             InitializeComponent();
+
+            On<Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
         }
     }
 }
