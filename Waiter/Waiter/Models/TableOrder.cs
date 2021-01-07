c9cc6aca53e87ce4bd1e40e2d15b1e3ae3adfbab
balauroiu.radu <balauroiu.radu@yahoo.com>
2021-01-07 23:11:13 +0200

    Update

--- Waiter/Waiter/Models/TableOrder.cs
+++ Waiter/Waiter/Models/TableOrder.cs
@@ -1,16 +1,39 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Waiter.Models
 {
-    public class TableOrder
+    public class TableOrder : INotifyPropertyChanged
     {
-        [JsonProperty("Order")]
+        private int count = 0;
+
         public MenuOrder Order { get; set; }
 
-        [JsonProperty("Count")]
-        public int Count { get; set; }
+        public int Count 
+        {
+            get { return count; }
+            set 
+            {
+                count = value;
+
+                OnPropertyChanged();
+            } 
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
