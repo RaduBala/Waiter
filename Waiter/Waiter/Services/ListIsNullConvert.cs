508b65277f74f9d12557dc14e14520095795b240
balauroiu.radu <balauroiu.radu@yahoo.com>
2021-01-13 21:00:45 +0200

    Update

---
+++ Waiter/Waiter/Services/ListIsNullConvert.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Text;
+using Waiter.Models;
+using Xamarin.Forms;
+
+namespace Waiter.Services
+{
+    public class ListIsNullConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ObservableCollection<OrderListItem> list = (ObservableCollection<OrderListItem>)value;
+
+            if (list.Count == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Waiter/Waiter/ViewModels/OrdersPageViewModel.cs
+++ Waiter/Waiter/ViewModels/OrdersPageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -8,13 +9,30 @@ using Waiter.Models;
 
 namespace Waiter.ViewModels
 {
-    public class OrdersPageViewModel
+    public class OrdersPageViewModel : INotifyPropertyChanged
     {
         public ObservableCollection<OrderListItem> OrderListItems {get; set;}
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public OrdersPageViewModel()
         {
             OrderListItems = new ObservableCollection<OrderListItem>();
+
+            OrderListItems.CollectionChanged += OrderListItems_CollectionChanged;
+        }
+
+        protected void OnPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        private void OrderListItems_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            OnPropertyChanged("OrderListItems");
         }
     }
 }
